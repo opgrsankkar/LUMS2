9361552a0dab0bf6219fdb57dd3a9a2227731c2e
Sankkara Narayanan <opgrsankkar@gmail.com>
2017-08-02 20:17:32 +0530

    Fix upload complete alert

--- dashboard/users/index.php
+++ dashboard/users/index.php
@@ -187,7 +187,7 @@ else {
                         </div>
                         <br>
                         <div class="alert alert-success alert-dismissible" role="alert"
-                             ng-show="usersTable.numberOfRecords && uploadComplete">
+                             ng-show="uploadComplete">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
                                         aria-hidden="true">&times;</span>
                             </button>
